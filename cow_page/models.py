9725b1bcd56351a0cfd4ade0d9f208cdcdc85b7f
ddeodeok <ejrgus0000@naver.com>
2022-09-06 22:46:26 +0900

    대쉬보드 부트스트랩 적용

--- cow_page/models.py
+++ cow_page/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
 # Create your models here.
+class Neck_sensor(models.Model):
+    sensor_id = models.CharField(max_length=50)
--- cow_page/views.py
+++ cow_page/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import get_object_or_404
+from django.core.exceptions import PermissionDenied
+from django.utils.text import slugify
+from django.db.models import Q
+
 
 # Create your views here.
+# class CowList(ListView):
+#     model = Neck_sensor
+#     paginate_by = 5
+#     template_name = 'cow_page/base.html'
+
+def cow (request):
+    return render(
+        request,
+        'cow_page/base.html'
+    )
+
+def charts (request):
+    return render(
+        request,
+        'cow_page/charts.html'
+    )
