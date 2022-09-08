6028066d581da7486827bcfc226f935ddf2933c1
ddeodeok <ejrgus0000@naver.com>
2022-09-08 17:51:54 +0900

    ㅡㅐ

--- cow_page/models.py
+++ cow_page/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 # Create your models here.
-class Neck_sensor(models.Model):
-    sensor_id = models.CharField(max_length=50)
+# class Neck_sensor(models.Model):
+#     sensor_id = models.CharField(max_length=50)
--- sensor_page/models.py
+++ sensor_page/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
 # Create your models here.
-class neck_sensor(models.Model):
-    sensor_id = models.CharFiled
+# class neck_sensor(models.Model):
+#     sensor_id = models.CharFiled
