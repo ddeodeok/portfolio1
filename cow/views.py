3bbaa6e88f364585a9b496f8abb5a3c9bee644df
ddeodeok <ejrgus0000@naver.com>
2022-09-15 00:12:29 +0900

    입력 완고쿠;;;

--- cow/views.py
+++ cow/views.py
@@ -5,7 +5,9 @@ from django.shortcuts import get_object_or_404
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
 from django.db.models import Q
-from .models import Cow, Sensor
+from .models import Cow, Sensor, Post
+import logging
+
 
 # Create your views here.
 
@@ -39,11 +41,31 @@ def sensorTables (request):
     )
 
 class CowCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    
     model = Cow
     template_name = 'cow/create_cow.html'
     fields=['cow_num','group','sensorID','age','stats','empyt_days','carving_num','birthday']
 
     def form_valid(self,form):
+        logger = logging.getLogger()
+
+        # 로그의 출력 기준 설정
+        logger.setLevel(logging.INFO)
+
+        # log 출력 형식
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+        # log 출력
+        stream_handler = logging.StreamHandler()
+        stream_handler.setFormatter(formatter)
+        logger.addHandler(stream_handler)
+
+        # log를 파일에 출력
+        file_handler = logging.FileHandler('my.log')
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+        logger.info('번째 방문입니다.')
+
         current_user = self.request.user
         if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
             form.instance.author = current_user
@@ -66,7 +88,38 @@ class CowCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
 #         post.save()
 #     return redirect('cow/create_cow.html')
 
+class PostCreate(CreateView):
+    model = Post
+    template_name = 'cow/post_list.html'
+    fields=['text','cow_num','group','stats','carving_num','age','empyt_days','birthday','sensorID','childbirth_id','estrus_id']
 
+
+
+
+
+
+
+
+
+# def post(request):
+#     if request.method == "POST":
+#         post = Post()
+#         post.text = request.POST['text']
+
+        
+#         post.cow_num = request.POST['cow_num']
+#         post.group = request.POST['group']
+#         post.stats = request.POST['stats']
+#         post.carving_num = request.POST['carving_num']
+#         post.age = request.POST['age']
+#         post.empyt_days = request.POST['empyt_days']
+#         post.birthday = request.POST['birthday']
+#         post.sensorID = request.POST['sensorID']
+#         post.save()
+#         return redirect('post')
+#     else:
+#         post = Post.objects.all()
+#         return render(request, 'cow/post_list.html', {'post':post})
 
 
 
